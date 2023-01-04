5ba2047656784f47527a16361c391aedad662921
Vasiliy Novak (Sabadazh) <vnovak2048@gmail.com>
2023-01-04 18:51:59 +0400

    initial commit

---
+++ iSplit/iSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSplitApp.swift
+//  iSplit
+//
+//  Created by Vasiliy Sabadazh on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct iSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
